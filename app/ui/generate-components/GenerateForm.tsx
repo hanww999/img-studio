0ce0091d94be52a9ddc57ca9a609edc190b8ad14
hanww999 <wei62804@gmail.com>
2025-08-13 17:23:03 +0800

    Update GenerateForm.tsx

--- app/ui/generate-components/GenerateForm.tsx
+++ app/ui/generate-components/GenerateForm.tsx
@@ -2,7 +2,6 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //   https://www.apache.org/licenses/LICENSE-2.0
@@ -722,9 +721,11 @@ export default function GenerateForm({
        }
       </Accordion>
      )}
+
       {/* 
         [MODIFICATION START] 
-        The entire "attributes" Accordion has been commented out to remove it from the UI.
+        The entire "attributes" Accordion has been commented out to remove it from the UI
+        for BOTH Image and Video generation modes.
       */}
       {/*
      <Accordion
