44ed7f3ac4688081cd0cb0d400c6d19ebc982d65
hanww999 <wei62804@gmail.com>
2025-08-14 15:04:56 +0800

    Update routes.tsx

--- app/routes.tsx
+++ app/routes.tsx
@@ -1,31 +1,15 @@
 // app/routes.tsx
-
-// Copyright 2025 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 export const pages = {
-  // FIX: Replaced the old 'Generate' with two new specific routes
   GenerateImage: {
     name: 'Generate an Image',
     description: 'Create new images from scratch or with references',
-    href: '/generate-image',
+    href: '/generate?mode=image', // FIX: Point to the same page with a URL parameter
     status: 'true',
   },
   GenerateVideo: {
     name: 'Generate a Video',
     description: 'Create new videos from text or images',
-    href: '/generate-video',
+    href: '/generate?mode=video', // FIX: Point to the same page with a URL parameter
     status: 'true',
   },
   Edit: {
