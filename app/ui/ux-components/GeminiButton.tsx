cd97f5d7b6530e622a04db43319dd675699aec5d
hanww999 <wei62804@gmail.com>
2025-08-18 21:05:15 +0800

    Update GeminiButton.tsx

--- app/ui/ux-components/GeminiButton.tsx
+++ app/ui/ux-components/GeminiButton.tsx
@@ -1,16 +1,4 @@
-// Copyright 2025 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
 
 import * as React from 'react'
 import { styled } from '@mui/material/styles'
@@ -95,7 +83,7 @@ export const GeminiSwitch = ({
   checked: boolean
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }) => {
-  return <MaterialUISwitch sx={{ mt: 2 }} checked={checked} onChange={onChange} />
+  return <MaterialUISwitch checked={checked} onChange={onChange} />
 }
 
 export const GeminiButton = ({ onClick }: { onClick: () => void }) => {
