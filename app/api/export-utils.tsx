765845d64ad92660ebf8570f754995e6234b4dc6
hanww999 <wei62804@gmail.com>
2025-08-19 13:53:18 +0800

    Update export-utils.tsx

--- app/api/export-utils.tsx
+++ app/api/export-utils.tsx
@@ -36,7 +36,7 @@ export interface ExportMediaFormFieldsI {
 
 export const exportStandardFields: ExportMediaFormFieldsI = {
  id: {
-  label: '媒体 ID', // [汉化]
+  label: '媒体 ID',
   type: 'text-info',
   prop: 'key',
   isUpdatable: false,
@@ -44,7 +44,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: false,
  },
  gcsURI: {
-  label: '媒体 GCS URI', // [汉化]
+  label: '媒体 GCS URI',
   type: 'text-info',
   prop: 'gcsUri',
   isUpdatable: false,
@@ -52,7 +52,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: false,
  },
  creationDate: {
-  label: '生成日期', // [汉化]
+  label: '生成日期',
   type: 'text-info',
   prop: 'date',
   isUpdatable: false,
@@ -60,7 +60,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  leveragedModel: {
-  label: '所用模型', // [汉化]
+  label: '所用模型',
   type: 'text-info',
   prop: 'modelVersion',
   isUpdatable: false,
@@ -68,7 +68,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  mediaCreationMode: {
-  label: '创建模式', // [汉化]
+  label: '创建模式',
   type: 'text-info',
   prop: 'mode',
   isUpdatable: false,
@@ -76,7 +76,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  author: {
-  label: '作者', // [汉化]
+  label: '作者',
   type: 'text-info',
   prop: 'author',
   isUpdatable: false,
@@ -84,7 +84,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  prompt: {
-  label: '提示词', // [汉化]
+  label: '提示词',
   type: 'text-info',
   prop: 'prompt',
   isUpdatable: false,
@@ -92,7 +92,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  format: {
-  label: '格式', // [汉化]
+  label: '格式',
   type: 'text-info',
   prop: 'format',
   isUpdatable: false,
@@ -100,7 +100,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  videoDuration: {
-  label: '时长 (秒)', // [汉化]
+  label: '时长 (秒)',
   type: 'text-info',
   prop: 'duration',
   isUpdatable: false,
@@ -108,7 +108,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  videoResolution: {
-  label: '分辨率', // [汉化]
+  label: '分辨率',
   type: 'text-info',
   prop: 'resolution',
   isUpdatable: false,
@@ -116,7 +116,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  videoThumbnailGcsUri: {
-  label: '视频缩略图 GCS URI', // [汉化]
+  label: '视频缩略图 GCS URI',
   type: 'text-info',
   prop: 'videoThumbnailGcsUri',
   isUpdatable: false,
@@ -124,7 +124,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: false,
  },
  aspectRatio: {
-  label: '宽高比', // [汉化]
+  label: '宽高比',
   type: 'text-info',
   prop: 'ratio',
   isUpdatable: false,
@@ -132,7 +132,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  upscaleFactor: {
-  label: '放大系数', // [汉化]
+  label: '放大系数',
   type: 'radio-button',
   prop: 'upscaleFactor',
   isUpdatable: false,
@@ -140,7 +140,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  width: {
-  label: '宽度 (像素)', // [汉化]
+  label: '宽度 (像素)',
   type: 'text-info',
   prop: 'width',
   isUpdatable: false,
@@ -148,7 +148,7 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
  height: {
-  label: '高度 (像素)', // [汉化]
+  label: '高度 (像素)',
   type: 'text-info',
   prop: 'height',
   isUpdatable: false,
@@ -156,6 +156,68 @@ export const exportStandardFields: ExportMediaFormFieldsI = {
   isExploreVisible: true,
  },
 }
+
+// [汉化] 媒体库筛选器选项
+export const exportMetaOptions = {
+  associatedTeams: {
+    name: 'associatedTeams',
+    label: '关联团队',
+    isExportVisible: true,
+    isExploreVisible: true,
+    isUpdatable: true,
+    options: [
+      { value: 'content_marketing', label: '内容营销' },
+      { value: 'community_management', label: '社区管理' },
+      { value: 'human_ressources', label: '人力资源' },
+      { value: 'product_development', label: '产品开发' },
+      { value: 'sales_enablement', label: '销售赋能' },
+    ],
+  },
+  targettedPlatforms: {
+    name: 'targettedPlatforms',
+    label: '目标平台',
+    isExportVisible: true,
+    isExploreVisible: true,
+    isUpdatable: true,
+    options: [
+      { value: 'mailing_campaign', label: '邮件营销' },
+      { value: 'public_website', label: '公共网站' },
+      { value: 'social_medias', label: '社交媒体' },
+      { value: 'product_development', label: '产品开发' },
+    ],
+  },
+  associatedBrands: {
+    name: 'associatedBrands',
+    label: '关联品牌',
+    isExportVisible: true,
+    isExploreVisible: true,
+    isUpdatable: true,
+    options: [
+      { value: 'gemo', label: 'Gémo' },
+      { value: 'mellow_yellow', label: 'Mellow Yellow' },
+      { value: 'bocage', label: 'Bocage' },
+      { value: 'maje', label: 'Maje' },
+      { value: 'ba_sh', label: 'Ba&sh' },
+      { value: 'sessun', label: 'Sessùn' },
+      { value: 'american_vintage', label: 'American Vintage' },
+      { value: 'the_kooples', label: 'The Kooples' },
+    ],
+  },
+  associatedCollections: {
+    name: 'associatedCollections',
+    label: '关联系列',
+    isExportVisible: true,
+    isExploreVisible: true,
+    isUpdatable: true,
+    options: [
+      { value: 'spring', label: '春季' },
+      { value: 'summer', label: '夏季' },
+      { value: 'fall', label: '秋季' },
+      { value: 'winter', label: '冬季' },
+    ],
+  },
+};
+
 
 export interface ExportMediaFormI {
  mediaToExport: ImageI | VideoI
